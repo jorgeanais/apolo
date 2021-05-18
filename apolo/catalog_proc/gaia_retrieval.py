cae3ab9ec1bb1719d4f95ffba3395c3f8b548d4d
jorgeanais <jrganais@gmail.com>
2021-05-18 12:42:04 -0400

    Added table parameter (support for gaia.edr3)

--- apolo/catalog_proc/gaia_retrieval.py
+++ apolo/catalog_proc/gaia_retrieval.py
@@ -4,13 +4,14 @@ from os import path
 from apolo.data import dirconfig
 
 
-def download_votable(tile, offset=0.005, output_dir=dirconfig.raw_gaia):
+def download_votable(tile, offset=0.005, output_dir=dirconfig.raw_gaia, gaia_source='gaiadr2.gaia_source'):
     """
     This function automatically download gaia-data from gaia-archive using AstroQuery module.
     tile: a Tile objects.
     Implemented query extract all the sources within the coordinates
     offset: is a safe-margin (in degrees) added to the borders of each tile
     output_dir : where vo-tables are saved
+    gaia_source: gaiaedr3.gaia_source or gaiadr2.gaia_source
     """
 
     print('---------------------------------------------')
@@ -18,7 +19,7 @@ def download_votable(tile, offset=0.005, output_dir=dirconfig.raw_gaia):
     lmax = tile.lmax + offset
     bmin = tile.bmin - offset
     bmax = tile.bmax + offset
-    query = f'SELECT * FROM gaiadr2.gaia_source WHERE l > {lmin:.4f} AND l < {lmax:.4f} AND b > {bmin:.4f} AND b < {bmax:.4f} '
+    query = f'SELECT * FROM {gaia_source} WHERE l > {lmin:.4f} AND l < {lmax:.4f} AND b > {bmin:.4f} AND b < {bmax:.4f} '
     print(query)
     file_path = path.join(output_dir, tile.name + '_gaia.vot.gz')
     t1 = time.time()
